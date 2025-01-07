a592c91dadaeaaa36732f1b40a1e883a1b5cb53d
Rizal Anggoro <gnoogler4@gmail.com>
2025-01-07 10:02:33 +0700

    feat: implement PATCH endpoint for updating environment details with validation and authentication

--- src/app/api/v1/environments/[environment_id]/route.ts
+++ src/app/api/v1/environments/[environment_id]/route.ts
@@ -87,3 +87,56 @@ export async function GET(
     return APIResponse.respondWithServerError();
   }
 }
+
+interface PATCHResponse {
+  success: boolean;
+  id: string;
+}
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { environment_id: string } }
+) {
+  try {
+    const json = await request.json();
+    const { name } = json;
+    const { environment_id: environmentId } = params;
+
+    // validasi request dari user
+    const validate = z
+      .object({
+        name: z
+          .string({ required_error: "Nama lingkungan tidak boleh kosong!" })
+          .min(1, "Nama lingkungan tidak boleh kosong!"),
+        environmentId: z
+          .string({ required_error: "ID lingkungan tidak boleh kosong!" })
+          .min(1, "ID lingkungan tidak boleh kosong!"),
+      })
+      .safeParse({ name, environmentId });
+    if (!validate.success)
+      return APIResponse.respondWithBadRequest(
+        validate.error.errors.map((it) => ({
+          path: it.path[0] as string,
+          message: it.message,
+        }))
+      );
+
+    // validasi bearer token
+    if (!verifyBearerToken(request))
+      return APIResponse.respondWithUnauthorized();
+
+    // update data lingkungan
+    const query = database
+      .updateTable("environments")
+      .set({ name })
+      .where("id", "=", environmentId)
+      .returning("id");
+    const result = await query.executeTakeFirstOrThrow();
+
+    return APIResponse.respondWithSuccess<PATCHResponse>({
+      success: true,
+      id: result.id,
+    });
+  } catch (e) {
+    return APIResponse.respondWithServerError();
+  }
+}
